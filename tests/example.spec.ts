6a95bc9b6c3b33de8772370d59ff924294a02ea7
DegtyarEO <geimerus@gmail.com>
2020-01-23 09:54:00 +0300

    Extract function

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,10 @@
+import { hello } from './testExample'
 import { expect } from 'chai'
 import 'mocha'
 
 describe('Check what test functionality works', () => {
   it('should return Hello', () => {
-    const result = 'Hello'
+    const result = hello()
     expect(result).to.equal('Hello')
   })
 })
