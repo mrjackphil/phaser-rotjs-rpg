5d30e6db24eefce81315c38c19f47d0594d78322
DegtyarEO <geimerus@gmail.com>
2020-01-23 17:30:23 +0300

    Update grid tests

--- src/grid.spec.ts
+++ src/grid.spec.ts
@@ -5,8 +5,32 @@ import { Grid } from './types'
 
 describe('Grid tests', () => {
   it('get column count', () => {
-    const grid: Grid = new GridManager(10, 10)
+    const grid: Grid = new GridManager(14, 10)
 
-    expect(grid.getColCount()).to.be.a('number')
+    expect(grid.getColCount()).to.be.eq(14)
+  })
+
+  it('get row count', () => {
+    const grid: Grid = new GridManager(13, 11)
+
+    expect(grid.getRowCount()).to.be.eq(11)
+  })
+
+  it('get tileSize default value', () => {
+    const grid: Grid = new GridManager(13, 11)
+
+    expect(grid.getTileSize()).to.be.eq(16)
+  })
+
+  it('get tileSize changed value', () => {
+    const grid: Grid = new GridManager(13, 11, 27)
+
+    expect(grid.getTileSize()).to.be.eq(27)
+  })
+
+  it('get grid cells count', () => {
+    const grid: Grid = new GridManager(5, 5)
+
+    expect(grid.getCellCount()).to.be.eq(25)
   })
 })
