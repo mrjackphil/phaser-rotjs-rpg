51f098c62229c4d074e275baf26cc04882fb5453
DegtyarEO <geimerus@gmail.com>
2020-01-23 17:09:36 +0300

    Update state tests

--- src/state.spec.ts
+++ src/state.spec.ts
@@ -3,8 +3,14 @@ import 'mocha'
 import StateManager from './state'
 
 describe('Test StateManager', () => {
-  it('State has solids', () => {
+  it('state has solids', () => {
     const state = new StateManager()
     expect(state.getSolids()).to.be.an("array")
+  })
+
+  it('can add solids', () => {
+    const state = new StateManager()
+    state.addSolid({ x: 0, y: 0})
+    expect(state.getSolids().length).to.be.eq(1)
   })
 })
