27535fa6e423c140c11a9dd5f60a00f19b22a3b4
DegtyarEO <geimerus@gmail.com>
2020-01-23 17:47:49 +0300

    Remove unused variables

--- src/state.ts
+++ src/state.ts
@@ -2,9 +2,6 @@ import { StateSystem, Vector } from "./types"
 
 export default class StateManager implements StateSystem {
   private solids: Vector[] = [];
-  gridWidth: number = 50;
-  gridHeight: number = 37;
-  gridSize: number = 16;
 
   public addSolid(o: Vector) {
     this.solids.push(o)
