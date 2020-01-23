8048087539f2762dc65aa657c13fcedb8fd9d575
DegtyarEO <geimerus@gmail.com>
2020-01-23 09:30:32 +0300

    Init testing

---
+++ tests/example.spec.ts
@@ -0,0 +1,11 @@
+import MapGenerator from '../src/map'
+import { expect } from 'chai'
+import 'mocha'
+
+
+describe('Hello function', () => {
+  it('should return Hello', () => {
+    const result = 'Hello'
+    expect(result).to.equal('Hello')
+  })
+})
