9a34a5f585de56390867729ce8ea629f05e4bd74
DegtyarEO <geimerus@gmail.com>
2020-01-24 14:15:59 +0300

    Add tests for random

--- src/random.spec.ts
+++ src/random.spec.ts
@@ -1,10 +1,27 @@
 import { expect } from 'chai'
 import 'mocha'
-import { generateRNGlocation } from './random'
+import { generateRNGlocation, getRandomNotSolidPosition } from './random'
+import { GridSystem, CollisionSystem } from './types'
 
-describe('Test function which generate random data', () => {
-  it(`${generateRNGlocation.name} return a valid vector with zeros`, () => {
+describe('Generate random data', () => {
+  it(`return a valid vector with zeros`, () => {
     const result = generateRNGlocation(0, 0)
     expect(result).to.deep.equal({ x: 0, y: 0 })
+  })
+
+  it(`return a valid vector with zeros`, () => {
+    const grid: GridSystem = {
+      getCellCount: () => 0,
+      getColCount: () => 5,
+      getRowCount: () => 5,
+      getTileSize: () => 0
+    }
+
+    const collision: CollisionSystem = {
+      isSolid: ({ value }) => !(value.x === 5 && value.y === 5)
+    }
+
+    const result = getRandomNotSolidPosition(grid, collision)
+    expect(result).to.deep.equal( { kind: 'grid', value: { x: 5, y: 5 } } )
   })
 })
