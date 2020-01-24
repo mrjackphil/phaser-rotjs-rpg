73e84980b631cc8ea8821db8f719934fd5d3eb18
DegtyarEO <geimerus@gmail.com>
2020-01-24 14:50:45 +0300

    Update random functions

--- src/random.spec.ts
+++ src/random.spec.ts
@@ -3,13 +3,45 @@ import 'mocha'
 import { generateRNGlocation, getRandomNotSolidPosition } from './random'
 import { GridSystem, CollisionSystem } from './types'
 
-describe('Generate random data', () => {
+describe('<Unstable>Generate random data', () => {
   it(`return a valid vector with zeros`, () => {
     const result = generateRNGlocation(0, 0)
     expect(result).to.deep.equal({ x: 0, y: 0 })
   })
 
-  it(`return a valid vector with zeros`, () => {
+  it(`return a non solid object`, () => {
+    const grid: GridSystem = {
+      getCellCount: () => 0,
+      getColCount: () => 5,
+      getRowCount: () => 5,
+      getTileSize: () => 0
+    }
+
+    const collision: CollisionSystem = {
+      isSolid: ({ value }) => !(value.x === 4 && value.y === 4)
+    }
+
+    const result = getRandomNotSolidPosition(grid, collision)
+    expect(result).to.deep.equal( { kind: 'grid', value: { x: 4, y: 4 } } )
+  })
+
+  it(`will generate zero vector`, () => {
+    const grid: GridSystem = {
+      getCellCount: () => 0,
+      getColCount: () => 0,
+      getRowCount: () => 0,
+      getTileSize: () => 0
+    }
+
+    const collision: CollisionSystem = {
+      isSolid: () => false
+    }
+
+    const result = getRandomNotSolidPosition(grid, collision)
+    expect(result).to.deep.equal( { kind: 'grid', value: { x: 0, y: 0 } } )
+  })
+
+  it('will fail', () => {
     const grid: GridSystem = {
       getCellCount: () => 0,
       getColCount: () => 5,
@@ -21,7 +53,6 @@ describe('Generate random data', () => {
       isSolid: ({ value }) => !(value.x === 5 && value.y === 5)
     }
 
-    const result = getRandomNotSolidPosition(grid, collision)
-    expect(result).to.deep.equal( { kind: 'grid', value: { x: 5, y: 5 } } )
+    expect(() => getRandomNotSolidPosition(grid, collision)).to.throw()
   })
 })
--- src/random.ts
+++ src/random.ts
@@ -8,12 +8,22 @@ export function generateRNGlocation(maxX: number, maxY: number): Vector {
   }
 }
 
-export function getRandomNotSolidPosition(grid: GridSystem, collision: CollisionSystem): GridVector {
-  const width = grid.getColCount()
-  const height = grid.getRowCount()
-  const vect = { kind: 'grid', value: generateRNGlocation(width, height) } as GridVector
+export function getRandomNotSolidPosition(grid: GridSystem, collision: CollisionSystem, count: number = 0): GridVector {
+  let width = grid.getColCount()
+  let height = grid.getRowCount()
+  width ? width-- : 0
+  height ? height-- : 0
+
+  const vect = {
+    kind: 'grid',
+    value: generateRNGlocation(width, height)
+  } as GridVector
+
+  if (count && count > 100) {
+    throw Error('Can\' find a not solid position. Function was called too many times')
+  }
 
   return collision.isSolid( vect )
-    ? getRandomNotSolidPosition(grid, collision)
+    ? getRandomNotSolidPosition(grid, collision, count + 1)
     : vect
 }
