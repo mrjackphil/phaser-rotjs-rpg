9918939f62f920acbdeda4b4e834a4977ea5aeb8
DegtyarEO <geimerus@gmail.com>
2020-01-27 09:51:25 +0300

    Remove movement tests and start again

--- src/player.spec.ts
+++ src/player.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import 'mocha'
 import Player from './player'
 import { InputSystem, CollisionSystem, RendererSystem, GridSystem } from './types'
+import { createGridVectorType } from './types_util'
 
 const inputMock: InputSystem = {
   isDown: () => false,
@@ -44,39 +45,5 @@ describe('Player Controller', () => {
     pl.update()
 
     expect(pl.element.x).to.be.eq(-1)
-  })
-
-  it('Moves down/right but right blocked', () => {
-    const tileSize = 16
-    const pl = new Player(
-      { ...inputMock, isRight: () => true, isDown: () => true },
-      { ...collisionMock, isEmpty: ({value}) => value.x !== 1 },
-      renderMock,
-      { ...gridMock, getTileSize: () => tileSize }
-    )
-
-    pl.element.x = tileSize
-    pl.element.y = tileSize
-    pl.speed = 2
-    pl.update()
-
-    expect(pl.element).to.be.contain( { x: tileSize, y: tileSize + 2 })
-  })
-
-  it('Moves down/right but down blocked', () => {
-    const tileSize = 16
-    const pl = new Player(
-      { ...inputMock, isRight: () => true, isDown: () => true },
-      { ...collisionMock, isEmpty: ({value}) => value.y !== 1 },
-      renderMock,
-      { ...gridMock, getTileSize: () => tileSize }
-    )
-
-    pl.element.x = tileSize
-    pl.element.y = tileSize
-    pl.speed = 2
-    pl.update()
-
-    expect(pl.element).to.be.contain( { x: tileSize + 2, y: tileSize })
   })
 })
