e8f762abd00a1f36c75a98ce55aa44bc29786803
DegtyarEO <geimerus@gmail.com>
2020-01-27 15:14:35 +0300

    Format function

--- src/gameobject.ts
+++ src/gameobject.ts
@@ -6,6 +6,7 @@ export default class GameObjectManager implements GameObjectSystem {
   public addObject(o: GameObjectEntity) {
     const maxID = this.objects.reduce( (acc, p) => acc > p.id ? acc : p.id, 0)
     const withID = { id: maxID + 1, ...o }
+
     this.objects.push(withID)
 
     return withID.id
