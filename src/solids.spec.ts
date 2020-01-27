2ba573db0798f7acb8e89717b190fd2879e84edd
DegtyarEO <geimerus@gmail.com>
2020-01-27 16:10:49 +0300

    Add dynamic external sources

--- src/solids.spec.ts
+++ src/solids.spec.ts
@@ -15,16 +15,47 @@ describe('Test StateManager', () => {
   })
 
   it('add additional solids', () => {
-    const solids = new SolidManager([[{x: 0, y: 0}]])
+    const externalSolidSource = {
+        solids: [{x: 1, y: 1 }],
+        getSolids: function() { return this.solids }
+    }
+    const extSolids = externalSolidSource.getSolids.bind(externalSolidSource)
+
+    const solids = new SolidManager([extSolids])
     solids.addSolid({ x: 1, y: 1 })
 
     expect(solids.getSolids().length).to.be.eq(2)
   })
 
   it('add two additional solids', () => {
-    const solids = new SolidManager([[{x: 0, y: 0}], [{ x: 1, y: 1 }]])
-    solids.addSolid({ x: 2, y: 2 })
+    const externalSolidSource = {
+        solids: [{x: 1, y: 1 }],
+        getSolids: function() { return this.solids }
+    }
+    const externalSolidSourc2 = {
+        solids: [{x: 2, y: 2 }],
+        getSolids: function() { return this.solids }
+    }
+    const extSolids = externalSolidSource.getSolids.bind(externalSolidSource)
+    const extSolid2 = externalSolidSourc2.getSolids.bind(externalSolidSourc2)
+
+    const solids = new SolidManager([extSolids, extSolid2])
+    solids.addSolid({ x: 1, y: 1 })
 
     expect(solids.getSolids().length).to.be.eq(3)
+  })
+
+  it('dynamic chenge external solid source', () => {
+    const externalSolidSource = {
+        solids: [{x: 1, y: 1 }],
+        getSolids: function() { return this.solids }
+    }
+    const extSolids = externalSolidSource.getSolids.bind(externalSolidSource)
+    const solids = new SolidManager([extSolids])
+    solids.addSolid({ x: 1, y: 1 })
+
+    externalSolidSource.solids = []
+
+    expect(solids.getSolids().length).to.be.eq(1)
   })
 })
--- src/solids.ts
+++ src/solids.ts
@@ -2,8 +2,9 @@ import { SolidStateSystem, Vector } from "./types"
 
 export default class SolidManager implements SolidStateSystem {
   private solids: Vector[] = [];
-  private additionals: Vector[][] = [];
-  constructor(additionalSolidStates?: Vector[][]) {
+  private additionals: Array<() => Vector[]>;
+
+  constructor(additionalSolidStates?: Array<() => Vector[]>) {
     this.additionals = additionalSolidStates || []
   }
 
@@ -12,7 +13,9 @@ export default class SolidManager implements SolidStateSystem {
   }
 
   public getSolids() {
-    const otherSolids = this.additionals.reduce( (acc, e) => acc.concat(e), [])
+    const otherSolids = this.additionals
+      .map( e => e() )
+      .reduce( (acc, e) => acc.concat(e), [])
     return this.solids.concat(otherSolids)
   }
 }
