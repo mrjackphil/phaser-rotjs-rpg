f6a6e76ca00faa44f35ee22f2911fde9cf742314
DegtyarEO <geimerus@gmail.com>
2020-01-27 15:42:19 +0300

    Rename solid manager variables

--- src/state.spec.ts
+++ src/state.spec.ts
@@ -1,16 +1,17 @@
 import { expect } from 'chai'
 import 'mocha'
-import StateManager from './state'
+import SolidManager from './state'
 
 describe('Test StateManager', () => {
   it('state has solids', () => {
-    const state = new StateManager()
-    expect(state.getSolids()).to.be.an("array")
+    const solids = new SolidManager()
+    expect(solids.getSolids()).to.be.an("array")
   })
 
   it('can add solids', () => {
-    const state = new StateManager()
-    state.addSolid({ x: 0, y: 0})
-    expect(state.getSolids().length).to.be.eq(1)
+    const solids = new SolidManager()
+    solids.addSolid({ x: 0, y: 0})
+    expect(solids.getSolids().length).to.be.eq(1)
+  })
   })
 })
