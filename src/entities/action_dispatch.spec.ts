12b1b67f53cda066ec631f27877c071233e272e7
DegtyarEO <geimerus@gmail.com>
2020-01-29 10:39:46 +0300

    Add action dispatcher

---
+++ src/entities/action_dispatch.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+import 'mocha'
+import ActionDispatcher from './action_dispatch'
+
+describe('Action dispatcher', () => {
+  it('get action', () => {
+    const dispatcher = new ActionDispatcher()
+    const action = dispatcher.getAction('open_door')
+    expect(action).to.contain.keys('action')
+  })
+
+  it('will cause error if action not exist', () => {
+    const dispatcher = new ActionDispatcher()
+    const action = () => dispatcher.getAction('some_strange_action')
+
+    expect(action).to.throw('Action not found')
+  })
+
+  it('if not valid parameters - will send error', () => {
+    const dispatcher = new ActionDispatcher()
+    const action = () =>
+        dispatcher.getAction('test_with_parameter', 'true')
+
+    expect(action).to.throw('Parameter is not valid')
+  })
+
+  it('if valid parameters - will return test action', () => {
+    const dispatcher = new ActionDispatcher()
+    const action =
+       dispatcher.getAction('test_with_parameter', true)
+
+    expect(action)
+        .to.be.deep
+        .contain({ key: 'test_with_parameter' } )
+  })
+})
---
+++ src/entities/action_dispatch.ts
@@ -0,0 +1,50 @@
+import {Action} from "../models/types"
+
+export default class ActionDispatcher {
+    private actions: Action[] = [open_door, test_action]
+
+    public getAction(key: string, params?: any) {
+        const action = this.getByKey(key)
+        const isValid = ActionDispatcher.paramValidation(action, params)
+        return isValid && action
+    }
+
+    private getByKey(key: string) {
+        const found = this.actions.filter(a => a.key === key)
+
+        if (found.length > 1) {
+            throw Error('Found too many actions')
+        }
+        if (found.length === 0) {
+            throw Error('Action not found')
+        }
+
+        return found[0]
+    }
+
+    private static paramValidation(
+        action: Action,
+        params?: any
+    ) {
+
+        if (!action.condition) { return true }
+
+        const isValid = action.condition(params)
+        if (!isValid) {
+            throw Error("Parameter is not valid")
+        }
+
+        return true
+    }
+}
+
+const test_action = {
+    key: 'test_with_parameter',
+    action: () => null,
+    condition: (params) => typeof params === "boolean"
+}
+
+const open_door = {
+    key: "open_door",
+    action: () => null,
+}
