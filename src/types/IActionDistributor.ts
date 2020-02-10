d482ea12343b3b8532159d0352b3171285e9ef3b
MrJackphil <geimerus@gmail.com>
2020-02-10 13:11:43 +0300

    Remove ActionParams type

--- src/types/IActionDistributor.ts
+++ src/types/IActionDistributor.ts
@@ -1,6 +1,6 @@
-import {Action, ActionParams} from "./types"
+import {Action} from "./types"
 import {ACTIONS} from "../entities/ActionDistributor"
 
 export default interface IActionDistributor {
-    getAction: (key: ACTIONS, params: ActionParams) => Action
+    getAction: (key: ACTIONS, params: unknown) => Action
 }
